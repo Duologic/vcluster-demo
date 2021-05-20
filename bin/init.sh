b6106e8240b4f0d159a56dd05b7f2c97aafdcae8
Duologic <jeroen@simplistic.be>
2021-05-20 09:26:01 +0200

    add to init script + fix import path

--- bin/init.sh
+++ bin/init.sh
@@ -8,5 +8,6 @@ tk init --k8s=false --inline --force
 jb install github.com/jsonnet-libs/k8s-alpha/"${VERSION}"
 jb install github.com/grafana/jsonnet-libs/ksonnet-util
 jb install github.com/grafana/jsonnet-libs/tanka-util
+jb install github.com/duologic/vcluster-libsonnet
 
 echo '(import "github.com/jsonnet-libs/k8s-alpha/'"${VERSION}"'/main.libsonnet")' > lib/k.libsonnet
--- environments/vcluster/main.jsonnet
+++ environments/vcluster/main.jsonnet
@@ -1,5 +1,5 @@
 local config = import '../../config.json';
-local vcluster = import 'github.com/duologic/vcluster-libsonnet/vcluster/main.libsonnet';
+local vcluster = import 'github.com/duologic/vcluster-libsonnet/main.libsonnet';
 local tk = import 'github.com/grafana/jsonnet-libs/tanka-util/main.libsonnet';
 
 {
